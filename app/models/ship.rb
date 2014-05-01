3e55f358dfbbf8da7032d568281ad2ffdef7e252
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-02 11:35:57 +1200

    Remove validation from Ship on player_id to enable creation in hand with Player

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,7 +1,7 @@
 class Ship < ActiveRecord::Base
   belongs_to :player
 
-  validates :player_id, :start_coordinate_x, :start_coordinate_y, :end_coordinate_x, :end_coordinate_y, :presence => true
+  validates :start_coordinate_x, :start_coordinate_y, :end_coordinate_x, :end_coordinate_y, :presence => true
   validates :start_coordinate_x, :start_coordinate_y, :end_coordinate_x, :end_coordinate_y, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than => 10 }
 
   def randomize(length)
