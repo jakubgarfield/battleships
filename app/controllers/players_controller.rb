431a31f257cc4793a97b3ad20cd8b1bab017b1ef
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-02 13:18:41 +1200

    Moved the view of existing players to the game view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,13 +12,11 @@ class PlayersController < ApplicationController
     game = Game.find(params[:game_id])
     
     player = game.players.build(params[:player].permit(:name))
-    player.active = game.players.length < 2
+    player.active = !game.players.any?(&:persisted?)
     player.build_random_ships
     player.save!
     
     flash[:error] = player.errors.full_messages.to_sentence unless game.save
     redirect_to game_player_path(game, player)
   end
-
-  protected
 end
