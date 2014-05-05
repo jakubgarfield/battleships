18e0725cfcd68546663cd0ffb67005a1ba3b0cae
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-05-05 12:15:23 +1200

    Few specs added

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe GamesController do
-
+  
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:game) do
+    Game.new()
+  end
+
+  def add_players(names)
+    names.each { |name| game.players.build(:name => name) }
+  end
+
+
+  describe "#waiting_for_players?" do
+    subject { game.waiting_for_players? }
+    
+    context "when no player is in the game" do 
+      it { should be true }
+    end  
+
+    context "when just one player entered a game" do 
+      before { add_players %w(john) }
+      it { should be true }
+    end
+
+    context "when two players are in the game" do
+      before { add_players %w(john, jim) }
+      it { should be false }
+    end
+  end
+
+  describe "finished?" do
+    context "when no player is in the game"
+    context "when nobody won yet"
+    context "when there is a winner"
+  end
+
+  describe "winner" do 
+    context "when no player is in the game"
+    context "when nobody won yet"
+    context "when there is a winner"
+  end
+    
 end
